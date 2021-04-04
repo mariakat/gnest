2cd37cf0ab40f530941326dfe6d356ba5401334b
MariaKat <mariakat80@gmail.com>
2021-04-04 16:53:46 +0300

    test

---
+++ oceanwp-child-theme-master/page-templates/about.php
@@ -0,0 +1,14 @@
+<?php /** Template Name: About Us*/
+get_header();?>
+
+<div class="wrapper">
+  <div class="container">
+    <h1>About Us</h1>
+    <div class="row">
+      <div class="col-md-3 sidebar">sidebar</div>
+      <div class="col-md-9 main">main</div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
